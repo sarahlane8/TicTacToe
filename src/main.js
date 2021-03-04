6fd1ec2f9d02dce22b34d7779a22cfac360380b3
Sarah Lane <sarahlane8@gmail.com>
2021-03-04 16:28:32 -0700

    Add functionality to reset button

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-on page load, it will say it's star's turn! at the top
-it will initiate a new game instance
-  whose turn is it
+// on page load, it will say it's star's turn! at the top
+// it will initiate a new game instance
+//   whose turn is it
+//
+// it will initiate 2 player instances
 
-it will initiate 2 player instances
+var resetButton = document.querySelector('.reset-btn');
 
-window.addEventListener('load', renderLocalStorageWins);
+resetButton.addEventListener('click', resetGames);
+
+// window.addEventListener('load', renderLocalStorageWins);
+
+
+function resetGames() {
+  //turns board empty
+  //deletes all wins from local storage
+  console.log("it works!");
+}
