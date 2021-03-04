6d22ee22d0cc1fe86c825e7b2f0a6ec98025e52b
Sarah Lane <sarahlane8@gmail.com>
2021-03-04 15:48:49 -0700

    Add reset button with styling

--- src/game.js
+++ src/game.js
@@ -6,26 +6,3 @@ class Game {
     //compare the game board against any winning solutions
   }
 }
-class Game {
-  constructor() {
-
-  }
-  checkForWinner() {
-    //compare the game board against any winning solutions
-  }
-}
-class Player {
-  constructor(id, token) {
-    this.id = id;
-    this.token = token;
-    this.wins = [];
-  }
-
-  saveWinsToStorage() {
-
-  }
-
-  retrieveWinsFromStorage() {
-
-  }
-}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 on page load, it will say it's star's turn! at the top
 it will initiate a new game instance
   whose turn is it
-  
+
 it will initiate 2 player instances
+
+window.addEventListener('load', renderLocalStorageWins);
